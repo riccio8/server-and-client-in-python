28c10dfc3af1d2c5a8c8e1ceefa7365a215e0088
0x90 <134144784+riccio8@users.noreply.github.com>
2024-09-01 15:19:57 +0200

    Update server.py

--- server.py
+++ server.py
@@ -21,7 +21,7 @@ while True:
     if (communication_socket != None):
         while True:
             message = communication_socket.recv(2048).decode("utf-8")
-            print("Il meessaggio dall'utente è: " + decodifica(message))
+            print("Il meessaggio dall'utente è: " + decode(message))
             communication_socket.send(f"Ho ricevuto il tuo messaggio!! Grassie".encode("utf-8"))
     time.sleep(60)
     communication_socket.close()
